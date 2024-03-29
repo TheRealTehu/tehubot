985e8071a21b5b7e9c0b7142cdb53ade8af83c37
TheRealTehu <levex11@gmail.com>
2024-03-29 15:51:43 +0100

    start project

---
+++ src/main/java/com/therealtehu/discordbot/TehuBot/TehuBotApplication.java
@@ -0,0 +1,13 @@
+package com.therealtehu.discordbot.TehuBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TehuBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TehuBotApplication.class, args);
+	}
+
+}
