d661bb5e0a9c756c09423a66b113fefd62be5c11
TheRealTehu <levex11@gmail.com>
2024-03-30 17:41:28 +0100

    fix ButtonLabel enum name

--- src/main/java/com/therealtehu/discordbot/TehuBot/model/button/ButtonLabel.java
+++ src/main/java/com/therealtehu/discordbot/TehuBot/model/button/ButtonLabel.java
@@ -1,13 +1,13 @@
 package com.therealtehu.discordbot.TehuBot.model.button;
 
-public enum ButtonLabels {
+public enum ButtonLabel {
     SET_SERVER_DEFAULT("setChannelsDefault"),
     CREATE_ONE_CHANNEL_FOR_ALL("createOneChannelForAll"),
     CREATE_CHANNELS_FOR_CATEGORIES("createChannelsForCategories"),
     SPECIFY_ONE_CHANNEL_FOR_ALL("specifyOneChannelForAll"),
     SPECIFY_CHANNELS_FOR_CATEGORIES("specifyChannelsForCategories");
     private final String buttonId;
-    ButtonLabels(String buttonId) {
+    ButtonLabel(String buttonId) {
         this.buttonId = buttonId;
     }
 
