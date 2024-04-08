0ab641771452200737673a51423d462fb50ede78
TheRealTehu <levex11@gmail.com>
2024-04-08 15:34:29 +0200

    refactor GetWikiCommand to avoid magic numbers

--- src/main/java/com/therealtehu/discordbot/TehuBot/model/command/GetWikiCommand.java
+++ src/main/java/com/therealtehu/discordbot/TehuBot/model/command/GetWikiCommand.java
@@ -8,24 +8,27 @@ import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.awt.*;
 import java.util.List;
 
+@Component
 public class GetWikiCommand extends CommandWithFunctionality {
+    private static final int MIN_TITLE_LENGTH = 1;
+    private static final int MAX_TITLE_LENGTH = 100;
+    private static final int MAX_MESSAGE_LENGTH = 4095;
     private static final OptionData TITLE_OPTION = new OptionData(
             OptionType.STRING,
             "wikititle",
             "The title of the wiki article you would like to find.",
-            true).setMinLength(1).setMaxLength(100);
+            true).setMinLength(MIN_TITLE_LENGTH).setMaxLength(MAX_TITLE_LENGTH);
 
     private static final String COMMAND_NAME = "getwiki";
-
     private static final String COMMAND_DESCRIPTION = "Get a wiki article.";
     private final WikiArticleService wikiArticleService;
-
-    private static final int MAX_MESSAGE_LENGTH = 4095;
-
+    @Autowired
     public GetWikiCommand(WikiArticleService wikiArticleService, MessageSender messageSender) {
         super(COMMAND_NAME, COMMAND_DESCRIPTION, List.of(TITLE_OPTION), messageSender);
         this.wikiArticleService = wikiArticleService;
