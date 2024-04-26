abc173050c5085e9acfcc8f595c710396c152bb1
TheRealTehu <levex11@gmail.com>
2024-04-26 15:51:11 +0200

    rename CommandConfiguration to ActionConfiguration

--- src/main/java/com/therealtehu/discordbot/TehuBot/model/action/ActionConfiguration.java
+++ src/main/java/com/therealtehu/discordbot/TehuBot/model/action/ActionConfiguration.java
@@ -1,11 +1,11 @@
-package com.therealtehu.discordbot.TehuBot.model.action.command;
+package com.therealtehu.discordbot.TehuBot.model.action;
 
 import net.dv8tion.jda.api.utils.messages.MessageCreateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class CommandConfiguration {
+public class ActionConfiguration {
     @Bean
     public MessageCreateBuilder getMessageCreateBuilder() {
         return new MessageCreateBuilder();
