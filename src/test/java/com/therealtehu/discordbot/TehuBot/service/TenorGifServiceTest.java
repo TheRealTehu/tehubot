c4a189cfe58c1b87fe8d6f9a6665dc9d153a5669
TheRealTehu <levex11@gmail.com>
2024-05-03 13:01:58 +0200

    create TenorGifServiceTest

---
+++ src/test/java/com/therealtehu/discordbot/TehuBot/service/TenorGifServiceTest.java
@@ -0,0 +1,12 @@
+package com.therealtehu.discordbot.TehuBot.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TenorGifServiceTest {
+
+    @Test
+    void getGifAsEmbed() {
+    }
+}
