c04e4aeaa31953a0bae28495839373f70d00c42f
TheRealTehu <levex11@gmail.com>
2024-05-13 15:33:07 +0200

    remove unused Mono mock from TenorGifServiceTest

--- src/test/java/com/therealtehu/discordbot/TehuBot/service/TenorGifServiceTest.java
+++ src/test/java/com/therealtehu/discordbot/TehuBot/service/TenorGifServiceTest.java
@@ -1,12 +1,57 @@
 package com.therealtehu.discordbot.TehuBot.service;
 
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 class TenorGifServiceTest {
+    private TenorGifService tenorGifService;
+    private final WebClient mockWebClient = Mockito.mock(WebClient.class);
+    private final WebClient.RequestHeadersUriSpec mockRequestHeaderUriSpec =
+            Mockito.mock(WebClient.RequestHeadersUriSpec.class);
+    private final WebClient.ResponseSpec mockResponseSpec = Mockito.mock(WebClient.ResponseSpec.class);
+
+    @BeforeEach
+    void setup() {
+        tenorGifService = new TenorGifService(mockWebClient);
+    }
 
     @Test
-    void getGifAsEmbed() {
+    void getGifAsEmbedWhenNormalSearchTermIsGivenThenCorrectMessageIsSent() {
+        when(mockWebClient.get()).thenReturn(mockRequestHeaderUriSpec);
+        when(mockRequestHeaderUriSpec.uri(anyString())).thenReturn(mockRequestHeaderUriSpec);
+        when(mockRequestHeaderUriSpec.retrieve()).thenReturn(mockResponseSpec);
+        when(mockResponseSpec.bodyToMono(String.class)).thenReturn(Mono.just(exampleJsonResponse));
+
+        MessageEmbed expected = new EmbedBuilder()
+                .setImage("https://example.com/example.gif")
+                .build();
+        MessageEmbed actual = tenorGifService.getGifAsEmbed("Test");
+
+        assertEquals(expected, actual);
     }
+
+    private final String exampleJsonResponse= """
+            {
+              "results": [
+                {
+                  "media_formats": {
+                    "gif": {
+                      "url": "https://example.com/example.gif"
+                    },
+                    "other_format": {
+                      "url": "https://example.com/other_format.mp4"
+                    }
+                  }
+                }
+              ]
+            }""";
 }
