ffa8d09ebeb37521c89ee90a3fe45d000e428d20
TheRealTehu <levex11@gmail.com>
2024-06-21 15:26:57 +0200

    add SteamRouletteData database entity

---
+++ src/main/java/com/therealtehu/discordbot/TehuBot/database/model/SteamRouletteData.java
@@ -0,0 +1,60 @@
+package com.therealtehu.discordbot.TehuBot.database.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class SteamRouletteData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    private Guild guild;
+
+    @OneToOne
+    private Member member;
+
+    private String gameTitle;
+
+    public SteamRouletteData() {
+    }
+
+    public SteamRouletteData(long id, Guild guild, Member member, String gameTitle) {
+        this.id = id;
+        this.guild = guild;
+        this.member = member;
+        this.gameTitle = gameTitle;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Guild getGuild() {
+        return guild;
+    }
+
+    public void setGuild(Guild guild) {
+        this.guild = guild;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public String getGameTitle() {
+        return gameTitle;
+    }
+
+    public void setGameTitle(String gameTitle) {
+        this.gameTitle = gameTitle;
+    }
+}
