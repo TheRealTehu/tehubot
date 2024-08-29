97100a59842a2df4c48e9d75384adca92dde8f2c
TheRealTehu <levex11@gmail.com>
2024-08-29 10:34:02 +0200

    add findByPublicId to PollRepository

--- src/main/java/com/therealtehu/discordbot/TehuBot/database/repository/poll/PollRepository.java
+++ src/main/java/com/therealtehu/discordbot/TehuBot/database/repository/poll/PollRepository.java
@@ -6,9 +6,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PollRepository extends JpaRepository<PollData, Long> {
     @Query(value = "SELECT p.id FROM poll_data p WHERE p.guild_id = :guildId ORDER BY p.id DESC LIMIT 1",
             nativeQuery = true)
     Long findLatestIdForGuild(@Param("guildId") long guildId);
+
+    Optional<PollData> findByPublicId(String publicId);
 }
