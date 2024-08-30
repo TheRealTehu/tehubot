75d1780082ee38f57bcf695fa4aa2eddbf81f335
TheRealTehu <levex11@gmail.com>
2024-08-30 13:22:26 +0200

    add MemberService

---
+++ src/main/java/com/therealtehu/discordbot/TehuBot/service/MemberService.java
@@ -0,0 +1,27 @@
+package com.therealtehu.discordbot.TehuBot.service;
+
+import com.therealtehu.discordbot.TehuBot.database.model.MemberData;
+import com.therealtehu.discordbot.TehuBot.database.repository.MemberRepository;
+import net.dv8tion.jda.api.entities.Member;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+    private MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public boolean addNewMemberIfNotExists(Member jpaMember) {
+        if(!memberRepository.existsByUserId(jpaMember.getIdLong())) {
+            MemberData memberData = new MemberData();
+            memberData.setUserId(jpaMember.getIdLong());
+            memberRepository.save(memberData);
+            return true;
+        }
+        return false;
+    }
+}
