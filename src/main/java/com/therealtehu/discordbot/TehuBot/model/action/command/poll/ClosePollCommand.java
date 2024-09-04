d5a07e660e85fb3a04c036246a9fed3307f6ec3c
TheRealTehu <levex11@gmail.com>
2024-09-04 11:57:53 +0200

    add ClosePollCommand

---
+++ src/main/java/com/therealtehu/discordbot/TehuBot/model/action/command/poll/ClosePollCommand.java
@@ -0,0 +1,11 @@
+package com.therealtehu.discordbot.TehuBot.model.action.command.poll;
+
+import com.therealtehu.discordbot.TehuBot.database.model.poll.PollData;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ClosePollCommand {
+    public void closePoll(PollData pollData) {
+
+    }
+}
