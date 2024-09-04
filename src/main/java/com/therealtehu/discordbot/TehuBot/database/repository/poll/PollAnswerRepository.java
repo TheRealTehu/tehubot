183a6b249d06933c89e3bb98b76d3cd17a37992e
TheRealTehu <levex11@gmail.com>
2024-09-04 11:22:29 +0200

    add query methods to PollAnswerRepository

--- src/main/java/com/therealtehu/discordbot/TehuBot/database/repository/poll/PollAnswerRepository.java
+++ src/main/java/com/therealtehu/discordbot/TehuBot/database/repository/poll/PollAnswerRepository.java
@@ -1,10 +1,19 @@
 package com.therealtehu.discordbot.TehuBot.database.repository.poll;
 
+import com.therealtehu.discordbot.TehuBot.database.model.MemberData;
 import com.therealtehu.discordbot.TehuBot.database.model.poll.PollAnswerData;
+import com.therealtehu.discordbot.TehuBot.database.model.poll.PollData;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PollAnswerRepository extends JpaRepository<PollAnswerData, Long> {
+    boolean existsByMemberDataAndPollData(MemberData memberData, PollData pollData);
+
+    int countByPollDataAndMemberData(PollData pollData, MemberData memberData);
+
+    Optional<PollAnswerData> findByPollDataAndAnswerEmoji(PollData pollData, String answerEmoji);
 
 }
