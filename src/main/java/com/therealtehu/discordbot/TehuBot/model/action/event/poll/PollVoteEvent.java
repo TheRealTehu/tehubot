37cd0b5bec0e18498803c892c5ccbe5d2abb7ded
TheRealTehu <levex11@gmail.com>
2024-09-16 13:52:50 +0200

    refactor PollVoteEvent to handle min role and double vote as expected

--- src/main/java/com/therealtehu/discordbot/TehuBot/model/action/event/poll/PollVoteEvent.java
+++ src/main/java/com/therealtehu/discordbot/TehuBot/model/action/event/poll/PollVoteEvent.java
@@ -8,9 +8,9 @@ import com.therealtehu.discordbot.TehuBot.database.repository.poll.PollRepositor
 import com.therealtehu.discordbot.TehuBot.model.action.command.poll.ClosePollCommand;
 import com.therealtehu.discordbot.TehuBot.model.action.event.EventHandler;
 import com.therealtehu.discordbot.TehuBot.model.action.event.EventName;
+import com.therealtehu.discordbot.TehuBot.service.display.MessageSender;
 import com.therealtehu.discordbot.TehuBot.service.poll.MessageReactionEventWithText;
 import com.therealtehu.discordbot.TehuBot.service.poll.PollAnswerService;
-import com.therealtehu.discordbot.TehuBot.service.display.MessageSender;
 import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.events.Event;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -84,13 +84,13 @@ public class PollVoteEvent extends EventHandler {
 
         MemberData memberData = optionalMember.get();
 
-        if(pollData.isAnonymous() && isDoubleVote(pollData, memberData)) {
-            pollAnswerService.removeVote(pollData, memberData);
+        if(pollData.isAnonymous() && isDoubleVote(pollData, memberData, reactionAddEvent.getEmoji().getAsReactionCode())) {
+            pollAnswerService.removeVote(pollData, memberData, reactionAddEvent.getEmoji().getAsReactionCode());
             return true;
         }
 
         if(!hasPermissionToVote(reactionAddEvent, pollData) || !hasRemainingVotes(memberData, pollData)) {
-            reactionAddEvent.getReaction().removeReaction().queue();
+            reactionAddEvent.getReaction().removeReaction(reactionAddEvent.getUser()).queue();
             return false;
         }
 
@@ -121,6 +121,10 @@ public class PollVoteEvent extends EventHandler {
         try {
             Role minimumRole = findGuildRoleByName(reactionAddEvent, pollData);
 
+            if(minimumRole.getPosition() == -1) {
+                return true;
+            }
+
             return reactionAddEvent.getMember().getRoles()
                     .stream()
                     .anyMatch(role -> role.getPosition() >= minimumRole.getPosition());
@@ -132,8 +136,8 @@ public class PollVoteEvent extends EventHandler {
         return false;
     }
 
-    private boolean isDoubleVote(PollData pollData, MemberData memberData) {
-        return pollAnswerService.voteExistsForMember(memberData, pollData);
+    private boolean isDoubleVote(PollData pollData, MemberData memberData, String answerEmoji) {
+        return pollAnswerService.voteExistsForMember(memberData, pollData, answerEmoji);
     }
 
     private Role findGuildRoleByName(MessageReactionEventWithText reactionAddEvent, PollData pollData) throws NoSuchElementException {
