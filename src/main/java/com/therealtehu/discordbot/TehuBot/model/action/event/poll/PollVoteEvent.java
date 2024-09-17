1a3e5dde8b31a64c7f59658794beb281d49137fc
TheRealTehu <levex11@gmail.com>
2024-09-17 13:11:03 +0200

    extract vote handling from PollVoteEvent

--- src/main/java/com/therealtehu/discordbot/TehuBot/model/action/event/poll/PollVoteEvent.java
+++ src/main/java/com/therealtehu/discordbot/TehuBot/model/action/event/poll/PollVoteEvent.java
@@ -1,45 +1,30 @@
 package com.therealtehu.discordbot.TehuBot.model.action.event.poll;
 
-import com.therealtehu.discordbot.TehuBot.database.model.MemberData;
-import com.therealtehu.discordbot.TehuBot.database.model.poll.PollAnswerData;
 import com.therealtehu.discordbot.TehuBot.database.model.poll.PollData;
-import com.therealtehu.discordbot.TehuBot.database.repository.MemberRepository;
 import com.therealtehu.discordbot.TehuBot.database.repository.poll.PollRepository;
-import com.therealtehu.discordbot.TehuBot.model.action.command.poll.ClosePollCommand;
 import com.therealtehu.discordbot.TehuBot.model.action.event.EventHandler;
 import com.therealtehu.discordbot.TehuBot.model.action.event.EventName;
 import com.therealtehu.discordbot.TehuBot.service.display.MessageSender;
 import com.therealtehu.discordbot.TehuBot.service.poll.MessageReactionEventWithText;
-import com.therealtehu.discordbot.TehuBot.service.poll.PollAnswerService;
 import com.therealtehu.discordbot.TehuBot.service.poll.PollUtil;
-import net.dv8tion.jda.api.entities.Role;
+import com.therealtehu.discordbot.TehuBot.service.poll.PollVoteHandler;
 import net.dv8tion.jda.api.events.Event;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
-import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.regex.Matcher;
 
 @Component
-public class PollVoteEvent extends EventHandler {
+public class PollVoteEvent extends EventHandler { //TODO: Check unit tests
     private final PollRepository pollRepository;
-    private final PollAnswerService pollAnswerService;
-    private final ClosePollCommand closePollCommand;
-    private final MemberRepository memberRepository;
+    private final PollVoteHandler pollVoteHandler;
 
     @Autowired
-    protected PollVoteEvent(MessageSender messageSender, PollRepository pollRepository,
-                            PollAnswerService pollAnswerService, ClosePollCommand closePollCommand,
-                            MemberRepository memberRepository) {
+    protected PollVoteEvent(MessageSender messageSender, PollRepository pollRepository, PollVoteHandler pollVoteHandler) {
         super(EventName.POLL_VOTE.getEventName(), messageSender);
         this.pollRepository = pollRepository;
-        this.pollAnswerService = pollAnswerService;
-        this.closePollCommand = closePollCommand;
-        this.memberRepository = memberRepository;
+        this.pollVoteHandler = pollVoteHandler;
     }
 
     @Override
@@ -49,7 +34,7 @@ public class PollVoteEvent extends EventHandler {
             if (matcher.find()) {
                 String pollId = matcher.group(1);
                 Optional<PollData> optionalPollData = pollRepository.findByPublicId(pollId);
-                optionalPollData.ifPresentOrElse(pollData -> handleVote(pollData, reactionAddEvent),
+                optionalPollData.ifPresentOrElse(pollData -> pollVoteHandler.handleVote(pollData, reactionAddEvent),
                         () -> messageSender.sendMessage(reactionAddEvent.getChannel().asTextChannel(),
                         "ERROR: Could not find poll"));
             } else {
@@ -57,92 +42,6 @@ public class PollVoteEvent extends EventHandler {
                         "ERROR: Could not find poll id");
             }
         }
-
-    }
-    @Transactional
-    public boolean handleVote(PollData pollData, MessageReactionEventWithText reactionAddEvent) {
-        if(pollData.isAnonymous()) {
-            reactionAddEvent.getReaction().clearReactions().queue();
-        }
-
-        if(pollData.isClosed()) {
-            return false;
-        }
-
-        if(pollData.getDeadLine() != null && pollData.getDeadLine().isBefore(OffsetDateTime.now(ZoneOffset.UTC))) {
-            closePollCommand.closePoll(pollData);
-            return false;
-        }
-
-        Optional<MemberData> optionalMember = memberRepository.findByUserId(reactionAddEvent.getUserIdLong());
-        if(optionalMember.isEmpty()) {
-            messageSender.sendMessage(reactionAddEvent.getChannel().asTextChannel(),
-                    "ERROR: Could not find user");
-            return false;
-        }
-
-        MemberData memberData = optionalMember.get();
-
-        if(pollData.isAnonymous() && isDoubleVote(pollData, memberData, reactionAddEvent.getEmoji().getAsReactionCode())) {
-            pollAnswerService.removeVote(pollData, memberData, reactionAddEvent.getEmoji().getAsReactionCode());
-            return true;
-        }
-
-        if(!hasPermissionToVote(reactionAddEvent, pollData) || !hasRemainingVotes(memberData, pollData)) {
-            reactionAddEvent.getReaction().removeReaction(reactionAddEvent.getUser()).queue();
-            return false;
-        }
-
-        Optional<PollAnswerData> optionalPollAnswerData = pollAnswerService
-                .getPollAnswerData(pollData, reactionAddEvent.getEmoji().getAsReactionCode());
-
-        if(optionalPollAnswerData.isPresent()) {
-            PollAnswerData answerData = optionalPollAnswerData.get();
-
-            answerData.addMember(memberData);
-            pollAnswerService.saveAnswer(answerData);
-
-            return true;
-        }
-
-        return false;
     }
 
-    private boolean hasRemainingVotes(MemberData memberData, PollData pollData) {
-        return pollAnswerService.countVotes(pollData, memberData) < pollData.getNumberOfVotesPerMember();
-    }
-
-    private boolean hasPermissionToVote(MessageReactionEventWithText reactionAddEvent, PollData pollData) {
-        if(pollData.getMinimumRole() == null) {
-            return true;
-        }
-
-        try {
-            Role minimumRole = findGuildRoleByName(reactionAddEvent, pollData);
-
-            if(minimumRole.getPosition() == -1) {
-                return true;
-            }
-
-            return reactionAddEvent.getMember().getRoles()
-                    .stream()
-                    .anyMatch(role -> role.getPosition() >= minimumRole.getPosition());
-
-        } catch (NoSuchElementException e) {
-            messageSender.sendMessage(reactionAddEvent.getChannel().asTextChannel(),
-                    "ERROR: Could not find role");
-        }
-        return false;
-    }
-
-    private boolean isDoubleVote(PollData pollData, MemberData memberData, String answerEmoji) {
-        return pollAnswerService.voteExistsForMember(memberData, pollData, answerEmoji);
-    }
-
-    private Role findGuildRoleByName(MessageReactionEventWithText reactionAddEvent, PollData pollData) throws NoSuchElementException {
-        return reactionAddEvent.getGuild()
-                .getRolesByName(pollData.getMinimumRole(), true)
-                .stream().findFirst()
-                .orElseThrow();
-    }
 }
