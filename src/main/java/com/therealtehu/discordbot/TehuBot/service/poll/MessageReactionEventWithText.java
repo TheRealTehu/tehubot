69255365cc4ff1c699e36a3b33e179b9959602d5
TheRealTehu <levex11@gmail.com>
2024-10-15 13:59:07 +0200

    refactor MessageReactionEventWithText to make message final

--- src/main/java/com/therealtehu/discordbot/TehuBot/service/poll/MessageReactionEventWithText.java
+++ src/main/java/com/therealtehu/discordbot/TehuBot/service/poll/MessageReactionEventWithText.java
@@ -3,7 +3,7 @@ package com.therealtehu.discordbot.TehuBot.service.poll;
 import net.dv8tion.jda.api.events.message.react.GenericMessageReactionEvent;
 
 public class MessageReactionEventWithText extends GenericMessageReactionEvent {
-    private String message;
+    private final String message;
 
     public MessageReactionEventWithText(GenericMessageReactionEvent event, String message) {
         super(event.getJDA(), event.getResponseNumber(), event.getUser(), event.getMember(),
