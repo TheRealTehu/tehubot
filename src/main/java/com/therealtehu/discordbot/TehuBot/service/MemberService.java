08c1b7e72c78fddda005b3d9574e379087c6f5fc
TheRealTehu <levex11@gmail.com>
2024-10-25 21:19:51 +0200

    add getMemberData function to MemberService

--- src/main/java/com/therealtehu/discordbot/TehuBot/service/MemberService.java
+++ src/main/java/com/therealtehu/discordbot/TehuBot/service/MemberService.java
@@ -2,10 +2,15 @@ package com.therealtehu.discordbot.TehuBot.service;
 
 import com.therealtehu.discordbot.TehuBot.database.model.MemberData;
 import com.therealtehu.discordbot.TehuBot.database.repository.MemberRepository;
+import com.therealtehu.discordbot.TehuBot.service.display.MessageSender;
+import com.therealtehu.discordbot.TehuBot.service.poll.MessageReactionEventWithText;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 public class MemberService {
@@ -17,7 +22,7 @@ public class MemberService {
     }
 
     public boolean addNewMemberIfNotExists(Member discordMember) {
-        if(!memberRepository.existsByUserId(discordMember.getIdLong())) {
+        if (!memberRepository.existsByUserId(discordMember.getIdLong())) {
             MemberData memberData = new MemberData();
             memberData.setUserId(discordMember.getIdLong());
             memberRepository.save(memberData);
@@ -31,5 +36,14 @@ public class MemberService {
                 .onSuccess(memberList -> memberList
                         .forEach(this::addNewMemberIfNotExists)
                 );
+    }
+
+    public MemberData getMemberData(long id) throws NoSuchElementException { //TODO: WRITE TESTS
+        Optional<MemberData> optionalMember = memberRepository.findByUserId(id);
+        if (optionalMember.isPresent()) {
+            return optionalMember.get();
+        } else {
+            throw new NoSuchElementException();
+        }
     }
 }
