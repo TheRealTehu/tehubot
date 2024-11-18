9b1277c9a54e905feae2123295f0981d08f99b76
TheRealTehu <levex11@gmail.com>
2024-11-18 12:51:09 +0100

    refactor SendGifCommand to store searchterm in database

--- src/main/java/com/therealtehu/discordbot/TehuBot/model/action/command/SendGifCommand.java
+++ src/main/java/com/therealtehu/discordbot/TehuBot/model/action/command/SendGifCommand.java
@@ -1,5 +1,9 @@
 package com.therealtehu.discordbot.TehuBot.model.action.command;
 
+import com.therealtehu.discordbot.TehuBot.database.model.GuildData;
+import com.therealtehu.discordbot.TehuBot.database.model.SendGifData;
+import com.therealtehu.discordbot.TehuBot.database.repository.GuildRepository;
+import com.therealtehu.discordbot.TehuBot.database.repository.SendGifRepository;
 import com.therealtehu.discordbot.TehuBot.service.TenorGifService;
 import com.therealtehu.discordbot.TehuBot.service.display.MessageSender;
 import net.dv8tion.jda.api.Permission;
@@ -17,6 +21,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Component
 public class SendGifCommand extends CommandWithFunctionality {
@@ -38,20 +44,26 @@ public class SendGifCommand extends CommandWithFunctionality {
     private static final String COMMAND_DESCRIPTION = "Send a gif from Tenor";
     private final TenorGifService tenorGifService;
     private final MessageCreateBuilder messageCreateBuilder;
+    private final GuildRepository guildRepository;
+    private final SendGifRepository sendGifRepository;
 
     @Autowired
-    public SendGifCommand(TenorGifService tenorGifService, MessageSender messageSender, MessageCreateBuilder messageCreateBuilder) {
+    public SendGifCommand(TenorGifService tenorGifService, MessageSender messageSender,
+                          MessageCreateBuilder messageCreateBuilder, GuildRepository guildRepository,
+                          SendGifRepository sendGifRepository) {
         super(COMMAND_NAME, COMMAND_DESCRIPTION, List.of(PROMPT_OPTION, CHANNEL_OPTION), messageSender);
         this.tenorGifService = tenorGifService;
         this.messageCreateBuilder = messageCreateBuilder;
+        this.guildRepository = guildRepository;
+        this.sendGifRepository = sendGifRepository;
     }
 
     @Override
     public void executeCommand(SlashCommandInteractionEvent event) {
+        event.deferReply().setEphemeral(true).queue();
+
         String prompt = event.getOption(OptionName.SEND_GIF_PROMPT_OPTION.getOptionName()).getAsString();
         OptionMapping channelOption = event.getOption(OptionName.SEND_GIF_CHANNEL_OPTION.getOptionName());
-
-        event.deferReply().setEphemeral(true).queue();
 
         TextChannel channel = event.getChannel().asTextChannel();
 
@@ -62,16 +74,36 @@ public class SendGifCommand extends CommandWithFunctionality {
         Member member = event.getMember();
 
         if (member.hasPermission(channel, Permission.MESSAGE_SEND)) {
-            MessageEmbed messageEmbed = tenorGifService.getGifAsEmbed(prompt);
-            MessageCreateData messageCreateData = messageCreateBuilder.addEmbeds(messageEmbed).build();
-            messageCreateBuilder.clear();
-
-            messageSender.sendMessage(channel, messageCreateData);
-
-            messageSender.sendMessageOnHook(event.getHook(), "Gif sent");
+            try {
+                saveToDatabase(event, prompt);
+                sendGifEmbed(event, prompt, channel);
+            } catch (NoSuchElementException e) {
+                messageSender.replyToEventEphemeral(event, e.getMessage());
+            }
         } else {
             messageSender.replyToEventEphemeral(event, "ERROR: Doesn't have permission to send message to channel!");
         }
 
+    }
+
+    private void sendGifEmbed(SlashCommandInteractionEvent event, String prompt, TextChannel channel) {
+        MessageEmbed messageEmbed = tenorGifService.getGifAsEmbed(prompt);
+        MessageCreateData messageCreateData = messageCreateBuilder.addEmbeds(messageEmbed).build();
+        messageCreateBuilder.clear();
+
+        messageSender.sendMessage(channel, messageCreateData);
+
+        messageSender.sendMessageOnHook(event.getHook(), "Gif sent");
+    }
+
+    private void saveToDatabase(SlashCommandInteractionEvent event, String searchedTerm) {
+        SendGifData sendGifData = new SendGifData();
+        sendGifData.setSearchTerm(searchedTerm);
+        Optional<GuildData> guildData = guildRepository.findById(event.getGuild().getIdLong());
+        if (guildData.isEmpty()) {
+            throw new NoSuchElementException("DATABASE ERROR: Guild not found!");
+        }
+        sendGifData.setGuild(guildData.get());
+        sendGifRepository.save(sendGifData);
     }
 }
--- src/test/java/com/therealtehu/discordbot/TehuBot/model/action/command/SendGifCommandTest.java
+++ src/test/java/com/therealtehu/discordbot/TehuBot/model/action/command/SendGifCommandTest.java
@@ -1,8 +1,13 @@
 package com.therealtehu.discordbot.TehuBot.model.action.command;
 
+import com.therealtehu.discordbot.TehuBot.database.model.GuildData;
+import com.therealtehu.discordbot.TehuBot.database.model.SendGifData;
+import com.therealtehu.discordbot.TehuBot.database.repository.GuildRepository;
+import com.therealtehu.discordbot.TehuBot.database.repository.SendGifRepository;
 import com.therealtehu.discordbot.TehuBot.service.TenorGifService;
 import com.therealtehu.discordbot.TehuBot.service.display.MessageSender;
 import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
@@ -21,12 +26,18 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
 import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 class SendGifCommandTest {
     private SendGifCommand sendGifCommand;
     @Mock
     private TenorGifService tenorGifServiceMock;
+    @Mock
+    private GuildRepository guildRepositoryMock;
+    @Mock
+    private SendGifRepository sendGifRepositoryMock;
     @Mock
     private MessageSender messageSenderMock;
     @Mock
@@ -53,22 +64,24 @@ class SendGifCommandTest {
     private InteractionHook interactionHookMock;
     @Mock
     private Member memberMock;
+    @Mock
+    private Guild guildMock;
+    @Mock
+    private GuildData guildDataMock;
     @BeforeEach
     void setup() {
-        sendGifCommand = new SendGifCommand(tenorGifServiceMock, messageSenderMock, messageCreateBuilderMock);
+        sendGifCommand = new SendGifCommand(tenorGifServiceMock, messageSenderMock, messageCreateBuilderMock,
+                guildRepositoryMock, sendGifRepositoryMock);
     }
-
+        //TODO: Rewrite tests to check DB use. Add test when DB use fails. First test is ok, can copy parts
     @Test
-    void executeCommandWhenNoChannelWasGivenAndUserHasPermissionSendsGifToSameChannel() {
-        when(eventMock.getOption("gifprompt")).thenReturn(gifPromptOptionMock);
-        when(gifPromptOptionMock.getAsString()).thenReturn("test");
-        when(eventMock.getOption("gifchannel")).thenReturn(null);
+    void executeCommandWhenNoChannelWasGivenAndUserHasPermissionAndGuildIsInDbSendsGifToSameChannelAndSavesToDb() {
         when(eventMock.deferReply()).thenReturn(replyCallbackMock);
         when(replyCallbackMock.setEphemeral(true)).thenReturn(replyCallbackMock);
 
-        when(tenorGifServiceMock.getGifAsEmbed("test")).thenReturn(messageEmbedMock);
-        when(messageCreateBuilderMock.addEmbeds(messageEmbedMock)).thenReturn(messageCreateBuilderMock);
-        when(messageCreateBuilderMock.build()).thenReturn(messageCreateDataMock);
+        when(eventMock.getOption(OptionName.SEND_GIF_PROMPT_OPTION.getOptionName())).thenReturn(gifPromptOptionMock);
+        when(gifPromptOptionMock.getAsString()).thenReturn("test");
+        when(eventMock.getOption(OptionName.SEND_GIF_CHANNEL_OPTION.getOptionName())).thenReturn(null);
 
         when(eventMock.getChannel()).thenReturn(messageChannelUnionMock);
         when(messageChannelUnionMock.asTextChannel()).thenReturn(textChannelMock);
@@ -76,6 +89,17 @@ class SendGifCommandTest {
         when(eventMock.getMember()).thenReturn(memberMock);
         when(memberMock.hasPermission(textChannelMock, Permission.MESSAGE_SEND)).thenReturn(true);
 
+        when(eventMock.getGuild()).thenReturn(guildMock);
+        when(guildMock.getIdLong()).thenReturn(1L);
+        when(guildRepositoryMock.findById(1L)).thenReturn(Optional.of(guildDataMock));
+        SendGifData expectedSendGifData = new SendGifData();
+        expectedSendGifData.setSearchTerm("test");
+        expectedSendGifData.setGuild(guildDataMock);
+
+        when(tenorGifServiceMock.getGifAsEmbed("test")).thenReturn(messageEmbedMock);
+        when(messageCreateBuilderMock.addEmbeds(messageEmbedMock)).thenReturn(messageCreateBuilderMock);
+        when(messageCreateBuilderMock.build()).thenReturn(messageCreateDataMock);
+
         when(eventMock.getHook()).thenReturn(interactionHookMock);
 
         sendGifCommand.executeCommand(eventMock);
@@ -83,33 +107,45 @@ class SendGifCommandTest {
         verify(eventMock).deferReply();
         verify(replyCallbackMock).setEphemeral(true);
         verify(replyCallbackMock).queue();
+
+        verify(guildRepositoryMock).findById(1L);
+        verify(sendGifRepositoryMock).save(expectedSendGifData);
+
+        verify(messageCreateBuilderMock).clear();
 
         verify(messageSenderMock).sendMessage(textChannelMock, messageCreateDataMock);
         verify(messageSenderMock).sendMessageOnHook(interactionHookMock, "Gif sent");
     }
 
     @Test
-    void executeCommandWhenChannelWasGivenAndUserHasPermissionSendsGifToThatChannel() {
-        TextChannel otherTextChannel = Mockito.mock(TextChannel.class);
-
-        when(eventMock.getOption("gifprompt")).thenReturn(gifPromptOptionMock);
-        when(gifPromptOptionMock.getAsString()).thenReturn("test");
-        when(eventMock.getOption("gifchannel")).thenReturn(channelOptionMock);
+    void executeCommandWhenChannelWasGivenAndUserHasPermissionAmdGuildIsInDbSendsGifToThatChannelAndSavesToDb() {
         when(eventMock.deferReply()).thenReturn(replyCallbackMock);
         when(replyCallbackMock.setEphemeral(true)).thenReturn(replyCallbackMock);
 
-        when(tenorGifServiceMock.getGifAsEmbed("test")).thenReturn(messageEmbedMock);
-        when(messageCreateBuilderMock.addEmbeds(messageEmbedMock)).thenReturn(messageCreateBuilderMock);
-        when(messageCreateBuilderMock.build()).thenReturn(messageCreateDataMock);
+        when(eventMock.getOption(OptionName.SEND_GIF_PROMPT_OPTION.getOptionName())).thenReturn(gifPromptOptionMock);
+        when(gifPromptOptionMock.getAsString()).thenReturn("test");
+        when(eventMock.getOption(OptionName.SEND_GIF_CHANNEL_OPTION.getOptionName())).thenReturn(channelOptionMock);
 
         when(eventMock.getChannel()).thenReturn(messageChannelUnionMock);
         when(messageChannelUnionMock.asTextChannel()).thenReturn(textChannelMock);
 
+        TextChannel otherTextChannelMock = Mockito.mock(TextChannel.class);
         when(channelOptionMock.getAsChannel()).thenReturn(guildChannelUnionMock);
-        when(guildChannelUnionMock.asTextChannel()).thenReturn(otherTextChannel);
+        when(guildChannelUnionMock.asTextChannel()).thenReturn(otherTextChannelMock);
 
         when(eventMock.getMember()).thenReturn(memberMock);
-        when(memberMock.hasPermission(otherTextChannel, Permission.MESSAGE_SEND)).thenReturn(true);
+        when(memberMock.hasPermission(otherTextChannelMock, Permission.MESSAGE_SEND)).thenReturn(true);
+
+        when(eventMock.getGuild()).thenReturn(guildMock);
+        when(guildMock.getIdLong()).thenReturn(1L);
+        when(guildRepositoryMock.findById(1L)).thenReturn(Optional.of(guildDataMock));
+        SendGifData expectedSendGifData = new SendGifData();
+        expectedSendGifData.setSearchTerm("test");
+        expectedSendGifData.setGuild(guildDataMock);
+
+        when(tenorGifServiceMock.getGifAsEmbed("test")).thenReturn(messageEmbedMock);
+        when(messageCreateBuilderMock.addEmbeds(messageEmbedMock)).thenReturn(messageCreateBuilderMock);
+        when(messageCreateBuilderMock.build()).thenReturn(messageCreateDataMock);
 
         when(eventMock.getHook()).thenReturn(interactionHookMock);
 
@@ -119,17 +155,23 @@ class SendGifCommandTest {
         verify(replyCallbackMock).setEphemeral(true);
         verify(replyCallbackMock).queue();
 
-        verify(messageSenderMock).sendMessage(otherTextChannel, messageCreateDataMock);
+        verify(guildRepositoryMock).findById(1L);
+        verify(sendGifRepositoryMock).save(expectedSendGifData);
+
+        verify(messageCreateBuilderMock).clear();
+
+        verify(messageSenderMock).sendMessage(otherTextChannelMock, messageCreateDataMock);
         verify(messageSenderMock).sendMessageOnHook(interactionHookMock, "Gif sent");
     }
 
     @Test
-    void executeCommandWhenNoChannelWasGivenAndUserDoesNotHavePermissionSendsErrorMessage() {
-        when(eventMock.getOption("gifprompt")).thenReturn(gifPromptOptionMock);
-        when(gifPromptOptionMock.getAsString()).thenReturn("test");
-        when(eventMock.getOption("gifchannel")).thenReturn(null);
+    void executeCommandWhenNoChannelWasGivenAndUserDoesNotHavePermissionSendsErrorMessageAndDoesNotSaveToDb() {
         when(eventMock.deferReply()).thenReturn(replyCallbackMock);
         when(replyCallbackMock.setEphemeral(true)).thenReturn(replyCallbackMock);
+
+        when(eventMock.getOption(OptionName.SEND_GIF_PROMPT_OPTION.getOptionName())).thenReturn(gifPromptOptionMock);
+        when(gifPromptOptionMock.getAsString()).thenReturn("test");
+        when(eventMock.getOption(OptionName.SEND_GIF_CHANNEL_OPTION.getOptionName())).thenReturn(null);
 
         when(eventMock.getChannel()).thenReturn(messageChannelUnionMock);
         when(messageChannelUnionMock.asTextChannel()).thenReturn(textChannelMock);
@@ -144,16 +186,19 @@ class SendGifCommandTest {
         verify(replyCallbackMock).queue();
 
         verifyNoInteractions(tenorGifServiceMock);
+        verifyNoInteractions(guildRepositoryMock);
+        verifyNoInteractions(sendGifRepositoryMock);
         verify(messageSenderMock).replyToEventEphemeral(eventMock, "ERROR: Doesn't have permission to send message to channel!");
 
     }
     @Test
-    void executeCommandWhenChannelWasGivenButUserDoesNotHavePermissionSendsErrorMessage() {
-        when(eventMock.getOption("gifprompt")).thenReturn(gifPromptOptionMock);
-        when(gifPromptOptionMock.getAsString()).thenReturn("test");
-        when(eventMock.getOption("gifchannel")).thenReturn(channelOptionMock);
+    void executeCommandWhenChannelWasGivenButUserDoesNotHavePermissionSendsErrorMessageAndDoesNotSaveToDb() {
         when(eventMock.deferReply()).thenReturn(replyCallbackMock);
         when(replyCallbackMock.setEphemeral(true)).thenReturn(replyCallbackMock);
+
+        when(eventMock.getOption(OptionName.SEND_GIF_PROMPT_OPTION.getOptionName())).thenReturn(gifPromptOptionMock);
+        when(gifPromptOptionMock.getAsString()).thenReturn("test");
+        when(eventMock.getOption(OptionName.SEND_GIF_CHANNEL_OPTION.getOptionName())).thenReturn(channelOptionMock);
 
         when(eventMock.getChannel()).thenReturn(messageChannelUnionMock);
         when(messageChannelUnionMock.asTextChannel()).thenReturn(textChannelMock);
@@ -171,6 +216,40 @@ class SendGifCommandTest {
         verify(replyCallbackMock).queue();
 
         verifyNoInteractions(tenorGifServiceMock);
+        verifyNoInteractions(guildRepositoryMock);
+        verifyNoInteractions(sendGifRepositoryMock);
         verify(messageSenderMock).replyToEventEphemeral(eventMock, "ERROR: Doesn't have permission to send message to channel!");
+    }
+
+    @Test
+    void executeCommandWhenNoChannelWasGivenAndUserHasPermissionAndGuildIsNotInDbSendsErrorMessageAndDoesNotSaveToDb() {
+        when(eventMock.deferReply()).thenReturn(replyCallbackMock);
+        when(replyCallbackMock.setEphemeral(true)).thenReturn(replyCallbackMock);
+
+        when(eventMock.getOption(OptionName.SEND_GIF_PROMPT_OPTION.getOptionName())).thenReturn(gifPromptOptionMock);
+        when(gifPromptOptionMock.getAsString()).thenReturn("test");
+        when(eventMock.getOption(OptionName.SEND_GIF_CHANNEL_OPTION.getOptionName())).thenReturn(null);
+
+        when(eventMock.getChannel()).thenReturn(messageChannelUnionMock);
+        when(messageChannelUnionMock.asTextChannel()).thenReturn(textChannelMock);
+
+        when(eventMock.getMember()).thenReturn(memberMock);
+        when(memberMock.hasPermission(textChannelMock, Permission.MESSAGE_SEND)).thenReturn(true);
+
+        when(eventMock.getGuild()).thenReturn(guildMock);
+        when(guildMock.getIdLong()).thenReturn(1L);
+        when(guildRepositoryMock.findById(1L)).thenReturn(Optional.empty());
+
+        sendGifCommand.executeCommand(eventMock);
+
+        verify(eventMock).deferReply();
+        verify(replyCallbackMock).setEphemeral(true);
+        verify(replyCallbackMock).queue();
+
+        verify(guildRepositoryMock).findById(1L);
+        verifyNoInteractions(sendGifRepositoryMock);
+
+        verify(messageSenderMock).replyToEventEphemeral(eventMock, "DATABASE ERROR: Guild not found!");
+        verifyNoMoreInteractions(messageSenderMock);
     }
 }
