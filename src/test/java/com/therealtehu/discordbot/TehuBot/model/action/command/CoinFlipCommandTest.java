ab53e3b7ce53e160a59db7e1fbb96db40f236406
TheRealTehu <levex11@gmail.com>
2024-11-28 11:21:53 +0100

    refactor CoinFlipCommand to check member permission

--- src/test/java/com/therealtehu/discordbot/TehuBot/model/action/command/CoinFlipCommandTest.java
+++ src/test/java/com/therealtehu/discordbot/TehuBot/model/action/command/CoinFlipCommandTest.java
@@ -6,6 +6,7 @@ import com.therealtehu.discordbot.TehuBot.database.repository.CoinFlipRepository
 import com.therealtehu.discordbot.TehuBot.database.repository.GuildRepository;
 import com.therealtehu.discordbot.TehuBot.service.display.MessageSender;
 import com.therealtehu.discordbot.TehuBot.utils.RandomNumberGenerator;
+import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
@@ -47,10 +48,11 @@ class CoinFlipCommandTest {
     }
 
     @Test
-    void executeCommandWhenRandomIs0ShouldWriteOutHead() {
+    void executeCommandWhenMemberHasPermissionAndRandomIs0WritesOutHeadAndSavesToDb() {
         String expectedMessage = "Member as mention has flipped a coin and it was HEAD!";
         CoinFlipData expectedCoinFlipData = new CoinFlipData(0L, guildDataMock, "Head");
 
+        when(memberMock.hasPermission(Permission.MESSAGE_SEND)).thenReturn(true);
         when(randomNumberGeneratorMock.getRandomNumber(100)).thenReturn(0);
         when(eventMock.getMember()).thenReturn(memberMock);
         when(memberMock.getAsMention()).thenReturn("Member as mention");
@@ -66,10 +68,11 @@ class CoinFlipCommandTest {
     }
 
     @Test
-    void executeCommandWhenRandomIsUnder49ShouldWriteOutHead() {
+    void executeCommandWhenMemberHasPermissionAndRandomIsUnder49WritesOutHeadAndSavesToDb() {
         String expectedMessage = "Member as mention has flipped a coin and it was HEAD!";
         CoinFlipData expectedCoinFlipData = new CoinFlipData(0L, guildDataMock, "Head");
 
+        when(memberMock.hasPermission(Permission.MESSAGE_SEND)).thenReturn(true);
         when(randomNumberGeneratorMock.getRandomNumber(100)).thenReturn(10);
         when(eventMock.getMember()).thenReturn(memberMock);
         when(memberMock.getAsMention()).thenReturn("Member as mention");
@@ -85,10 +88,11 @@ class CoinFlipCommandTest {
     }
 
     @Test
-    void executeCommandWhenRandomIs48ShouldWriteOutHead() {
+    void executeCommandWhenMemberHasPermissionAndRandomIs48WritesOutHeadAndSavesToDb() {
         String expectedMessage = "Member as mention has flipped a coin and it was HEAD!";
         CoinFlipData expectedCoinFlipData = new CoinFlipData(0L, guildDataMock, "Head");
 
+        when(memberMock.hasPermission(Permission.MESSAGE_SEND)).thenReturn(true);
         when(randomNumberGeneratorMock.getRandomNumber(100)).thenReturn(48);
         when(eventMock.getMember()).thenReturn(memberMock);
         when(memberMock.getAsMention()).thenReturn("Member as mention");
@@ -104,10 +108,11 @@ class CoinFlipCommandTest {
     }
 
     @Test
-    void executeCommandWhenRandomIs49ShouldWriteOutTail() {
+    void executeCommandWhenMemberHasPermissionAndRandomIs49WritesOutTailAndSavesToDb() {
         String expectedMessage = "Member as mention has flipped a coin and it was TAIL!";
         CoinFlipData expectedCoinFlipData = new CoinFlipData(0L, guildDataMock, "Tail");
 
+        when(memberMock.hasPermission(Permission.MESSAGE_SEND)).thenReturn(true);
         when(randomNumberGeneratorMock.getRandomNumber(100)).thenReturn(49);
         when(eventMock.getMember()).thenReturn(memberMock);
         when(memberMock.getAsMention()).thenReturn("Member as mention");
@@ -123,11 +128,11 @@ class CoinFlipCommandTest {
     }
 
     @Test
-    void executeCommandWhenRandomIsUnder98ShouldWriteOutTail() {
+    void executeCommandWhenMemberHasPermissionAndRandomIsUnder98WritesOutTailAndSavesToDb() {
         String expectedMessage = "Member as mention has flipped a coin and it was TAIL!";
         CoinFlipData expectedCoinFlipData = new CoinFlipData(0L, guildDataMock, "Tail");
 
-
+        when(memberMock.hasPermission(Permission.MESSAGE_SEND)).thenReturn(true);
         when(randomNumberGeneratorMock.getRandomNumber(100)).thenReturn(67);
         when(eventMock.getMember()).thenReturn(memberMock);
         when(memberMock.getAsMention()).thenReturn("Member as mention");
@@ -143,10 +148,11 @@ class CoinFlipCommandTest {
     }
 
     @Test
-    void executeCommandWhenRandomIs98ShouldWriteOutSide() {
+    void executeCommandWhenMemberHasPermissionAndRandomIs98WritesOutSideAndSavesToDb() {
         String expectedMessage = "Member as mention has flipped a coin and the coin STOOD ON IT'S SIDE!";
         CoinFlipData expectedCoinFlipData = new CoinFlipData(0L, guildDataMock, "Side");
 
+        when(memberMock.hasPermission(Permission.MESSAGE_SEND)).thenReturn(true);
         when(randomNumberGeneratorMock.getRandomNumber(100)).thenReturn(98);
         when(eventMock.getMember()).thenReturn(memberMock);
         when(memberMock.getAsMention()).thenReturn("Member as mention");
@@ -162,10 +168,11 @@ class CoinFlipCommandTest {
     }
 
     @Test
-    void executeCommandWhenRandomIs99ShouldWriteOutLost() {
+    void executeCommandWhenMemberHasPermissionAndRandomIs99WritesOutLostAndSavesToDb() {
         String expectedMessage = "Member as mention has flipped a coin and the coin bounced behind the couch, you can't find it!";
         CoinFlipData expectedCoinFlipData = new CoinFlipData(0L, guildDataMock, "Lost");
 
+        when(memberMock.hasPermission(Permission.MESSAGE_SEND)).thenReturn(true);
         when(randomNumberGeneratorMock.getRandomNumber(100)).thenReturn(99);
         when(eventMock.getMember()).thenReturn(memberMock);
         when(memberMock.getAsMention()).thenReturn("Member as mention");
@@ -181,10 +188,11 @@ class CoinFlipCommandTest {
     }
 
     @Test
-    void executeCommandWhenRandomIsOver99ShouldWriteOutLost() {
+    void executeCommandWhenMemberHasPermissionAndRandomIsOver99WritesOutLostAndSavesToDb() {
         String expectedMessage = "Member as mention has flipped a coin and the coin bounced behind the couch, you can't find it!";
         CoinFlipData expectedCoinFlipData = new CoinFlipData(0L, guildDataMock, "Lost");
 
+        when(memberMock.hasPermission(Permission.MESSAGE_SEND)).thenReturn(true);
         when(randomNumberGeneratorMock.getRandomNumber(100)).thenReturn(110);
         when(eventMock.getMember()).thenReturn(memberMock);
         when(memberMock.getAsMention()).thenReturn("Member as mention");
@@ -200,8 +208,10 @@ class CoinFlipCommandTest {
     }
 
     @Test
-    void executeCommandWhenGuildIsNotInDatabaseShouldWriteOutErrorMessage() {
+    void executeCommandWhenMemberHasPermissionButGuildIsNotInDatabaseWritesOutErrorMessageAndDoesNotSaveToDb() {
         String expectedMessage = "DATABASE ERROR: Guild not found!";
+
+        when(memberMock.hasPermission(Permission.MESSAGE_SEND)).thenReturn(true);
         when(randomNumberGeneratorMock.getRandomNumber(100)).thenReturn(0);
         when(eventMock.getMember()).thenReturn(memberMock);
         when(memberMock.getAsMention()).thenReturn("Member as mention");
@@ -214,5 +224,17 @@ class CoinFlipCommandTest {
         verify(messageSenderMock).reply(eventMock, expectedMessage);
         verify(guildRepositoryMock).findById(1L);
         verify(coinFlipRepositoryMock, times(0)).save(any());
+    }
+
+    @Test
+    void executeCommandWhenMemberDoesNotHavePermissionDoesNothing() {
+        when(eventMock.getMember()).thenReturn(memberMock);
+        when(memberMock.hasPermission(Permission.MESSAGE_SEND)).thenReturn(false);
+
+        coinFlipCommand.executeCommand(eventMock);
+
+        verifyNoInteractions(messageSenderMock);
+        verifyNoInteractions(guildRepositoryMock);
+        verifyNoInteractions(randomNumberGeneratorMock);
     }
 }
